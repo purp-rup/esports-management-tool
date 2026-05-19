1a2b63b2235686710ee830998796b8966e4069a3
purp-rup <miragliaandrew@gmail.com>
2026-05-19 14:12:41 -0400

    Created an Example Test Case
    - conftest.py acts as the config file for testing, so any functions that exist there are global across all test files
    - test_example.py shows a simple test case, using the response from a client call to the /login route
    - Added an additional if-block to the set_mysql_timezone() function, so I made __init__.py up-to-date with the current main branch to avoid merge conflicts

--- EsportsManagementTool/tests/conftest.py
+++ EsportsManagementTool/tests/conftest.py
@@ -1,5 +1,13 @@
 import pytest
-from EsportsManagementTool import app
+from EsportsManagementTool import app as application
+
+@pytest.fixture()
+def app():
+    app = application
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
 
 @pytest.fixture()
 def client(app):
@@ -9,14 +17,3 @@ def client(app):
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
-
-
-def test_request_example(client):
-    response = client.get("/login")
-    assert response.status_code == 200
-
-
-def test_all_routes_accessible(client):
-    for rule in app.url_map.iter_rules():
-        response = client.get(rule.rule) # e.g. "/login"
-        assert response.status_code == 200
---
+++ EsportsManagementTool/tests/test_example.py
@@ -0,0 +1,7 @@
+from conftest import *
+from EsportsManagementTool import app as application
+
+# Checks if the login page is accessible and returns a 200 status code
+def test_request_example(client):
+    response = client.get("/login")
+    assert response.status_code == 200
