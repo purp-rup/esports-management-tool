08088888078d4d79f61769275092f15758181d97
purp-rup <miragliaandrew@gmail.com>
2026-07-24 22:49:28 -0400

    Made mail vars env vars

--- EsportsManagementTool/EsportsManagementTool/__init__.py
+++ EsportsManagementTool/EsportsManagementTool/__init__.py
@@ -48,9 +48,9 @@ app.config['MYSQL_CUSTOM_OPTIONS'] = {
 
 # Email Configuration (Brevo SMTP)
 class ProductionEmailConfig:
-    MAIL_SERVER = 'smtp-relay.brevo.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
